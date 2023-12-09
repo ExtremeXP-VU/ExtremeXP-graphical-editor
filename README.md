41837731ece461e70f073e0aa306872b19c5ac4c
Yuna C <yunabell@outlook.com>
2023-12-09 20:32:48 +0100

    add frontend dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ExtremeXP-graphical-editor
+
+## Build
+
+In the root folder (i.e. the folder contains `docker-compose.yaml` file):
+
+```shell
+# make sure docker is installed
+docker-compose up -d
+```
+
+To access the app: http://localhost:7001/
--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 7000, // you can replace this port with any port
+    watch: {
+      usePolling: true,
+    },
+  },
+});
