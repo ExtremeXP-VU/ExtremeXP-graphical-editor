a37d200cf3d1f4de32c59283f0ad722eef1485eb
Yuna C <yunabell@outlook.com>
2024-06-29 11:45:06 +0200

    develop version

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
-// import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 8080, // you can replace this port with any port
-  //   watch: {
-  //     usePolling: true,
-  //   },
-  // },
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url)),
-  //   },
-  // },
+  server: {
+    host: '0.0.0.0',
+    port: 7001, // you can replace this port with any port
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
